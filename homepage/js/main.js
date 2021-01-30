c4076be4352613803e66c703a480287247144722
nickgaur <nikhilgaur3@rocketmail.com>
2021-01-31 00:08:24 +0530

    uploaded

--- homepage/js/main.js
+++ homepage/js/main.js
@@ -78,3 +78,16 @@
         equalTo: ""
     });
 })(jQuery);
+
+
+
+// CLOSE BUTTON
+$(".menu-icon").click(function(){
+  $(this).css("display","none");
+    $(".close").css("display","block");
+});
+
+$(".close").click(function(){
+  $(this).css("display", "none");
+  $(".menu-icon").css("display", "block");
+});
